cb6f2e34b0b1095063b08d24a80087b4fb98cb62
shuvo794 <shuvohossain794@gmail.com>
2022-03-10 01:13:19 +0600

    updete

--- src/App.js
+++ src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Read from "./components/Read/Read";
+import NotFound from "./components/NotFound/NotFound";
+import Create from "./components/Create/Create";
+import { UserProvider } from "./components/UserContext/UserContext";
+import Delete from "./components/Delete/Delete";
+import Edit from "./components/Edit/Edit";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          start App
-        </a>
-      </header>
-    </div>
+    <UserProvider> 
+      <div className="app">
+        <Router>
+          <Switch>
+            <Route path="/create">
+              <Create />
+            </Route>
+            <Route path="/read/:id">
+              <Read />
+            </Route>
+            <Route path="/edit/:id">
+              <Edit />
+            </Route>
+            <Route path="/delete/:id">
+              <Delete />
+            </Route>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="*">
+              <NotFound />
+            </Route>
+          </Switch>
+        </Router>
+      </div>
+    </UserProvider>
   );
 }
 
